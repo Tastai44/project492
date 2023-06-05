bee98af72b7fe4508c48200431e5691adc36fa62
Tastai44 <tastai_kh@cmu.ac.th>
2023-06-05 15:53:09 +0700

    Initial setup firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,151 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import HomeFeed from "./pages/HomeFeed";
+import Members from "./pages/Members";
+import Topics from "./pages/Topics";
+import Groups from "./pages/Group/Groups";
+import Events from "./pages/Events/Events";
+import Blog from "./pages/Profile/Blog";
+import AboutMe from "./pages/Profile/AboutMe";
+import Friends from "./pages/Profile/Friends";
+import Collections from "./pages/Profile/Collections";
+import EventDetail from "./pages/Events/EventDetail";
+import GroupDetails from "./pages/Group/GroupDetails";
+import Login from "./pages/Login";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
-import {Routes, Route} from "react-router-dom"
-import './App.css'
-import Navigation from "./components/Navigation"
-import HomeFeed from "./pages/HomeFeed"
-import Members from "./pages/Members"
-import Topics from "./pages/Topics"
-import Groups from "./pages/Group/Groups"
-import Events from "./pages/Events/Events"
-import Blog from "./pages/Profile/Blog"
-import AboutMe from "./pages/Profile/AboutMe"
-import Friends from "./pages/Profile/Friends"
-import Collections from "./pages/Profile/Collections"
-import EventDetail from "./pages/Events/EventDetail"
-import GroupDetails from "./pages/Group/GroupDetails"
+// import { auth } from "./config/firebase"
 
 function App() {
-
+  // User is authenticated, render the component
   return (
     <>
       <Navigation />
-        <Routes>
-          <Route  path={"/"} element={<HomeFeed />} />
-          <Route path={"/members"} element={<Members />} />
-          <Route path={"/topics"} element={<Topics />} />
-          
-          <Route path={"/profileBlog/:userId"} element={<Blog />} />
-          <Route path={"/aboutMe/:userId"} element={<AboutMe />} />
-          <Route path={"/friends/:userId"} element={<Friends />} />
-          <Route path={"/collections/:userId"} element={<Collections />} />
+      <Routes>
+        <Route path={"/login"} element={<Login />} />
 
-          <Route path={"/events"} element={<Events />} />
-          <Route path={"/eventsDetail/:eventId"} element={<EventDetail />} />
-          
-          <Route path={"/groups"} element={<Groups />} />
-          <Route path={"/groupDetail/:groupId"} element={<GroupDetails />} />
+        <Route
+          path={"/"}
+          element={
+            <>
+              <ProtectedRoute>
+                <HomeFeed />
+              </ProtectedRoute>
+            </>
+          }
+        />
+        <Route
+          path={"/members"}
+          element={
+            <>
+              <ProtectedRoute>
+                <Members />
+              </ProtectedRoute>
+            </>
+          }
+        />
+        <Route
+          path={"/topics"}
+          element={
+            <>
+              <ProtectedRoute>
+                <Topics />
+              </ProtectedRoute>
+            </>
+          }
+        />
+        {/* Profile */}
+        <Route
+          path={"/profileBlog/:userId"}
+          element={
+            <>
+              <ProtectedRoute>
+                <Blog />
+              </ProtectedRoute>
+            </>
+          }
+        />
+        <Route
+          path={"/aboutMe/:userId"}
+          element={
+            <>
+              <ProtectedRoute>
+                <AboutMe />
+              </ProtectedRoute>
+            </>
+          }
+        />
+        <Route
+          path={"/friends/:userId"}
+          element={
+            <>
+              <ProtectedRoute>
+                <Friends />
+              </ProtectedRoute>
+            </>
+          }
+        />
+        <Route
+          path={"/collections/:userId"}
+          element={
+            <>
+              <ProtectedRoute>
+                <Collections />
+              </ProtectedRoute>
+            </>
+          }
+        />
+        {/* Profile */}
 
-        </Routes>
+        {/* Event */}
+        <Route
+          path={"/events"}
+          element={
+            <>
+              <ProtectedRoute>
+                <Events />
+              </ProtectedRoute>
+            </>
+          }
+        />
+        <Route
+          path={"/eventsDetail/:eventId"}
+          element={
+            <>
+              <ProtectedRoute>
+                <EventDetail />
+              </ProtectedRoute>
+            </>
+          }
+        />
+        {/* Event */}
+
+        {/* Groups */}
+        <Route
+          path={"/groups"}
+          element={
+            <>
+              <ProtectedRoute>
+                <Groups />
+              </ProtectedRoute>
+            </>
+          }
+        />
+        <Route
+          path={"/groupDetail/:groupId"}
+          element={
+            <>
+              <ProtectedRoute>
+                <GroupDetails />
+              </ProtectedRoute>
+            </>
+          }
+        />
+        {/* Groups */}
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,134 @@
+import {
+  Box,
+  Button,
+  CardMedia,
+  Grid,
+  Paper,
+  TextField,
+  styled,
+} from "@mui/material";
+import * as React from "react";
+// import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
+import community from "../../public/pictures/communityPic.png";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  height: "600px",
+}));
+
+export default function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  // const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+  //     e.preventDefault();
+  //     try {
+  //       await createUserWithEmailAndPassword(auth, email, password);
+  //       console.log("User created successfully!");
+  //     } catch (error) {
+  //       console.error("Error creating user:", error);
+  //     }
+  //   };
+  const handleSignIn = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      console.error("Error login user:", error);
+    }
+  };
+
+  return (
+    // <Box sx={{ mt: 8 }}>
+    //   <TextField
+    //     id="email"
+    //     label="Email"
+    //     variant="outlined"
+    //     type="email"
+    //     value={email}
+    //     onChange={(e) => setEmail(e.target.value)}
+    //   />
+    //   <TextField
+    //     id="password"
+    //     label="Password"
+    //     variant="outlined"
+    //     type="password"
+    //     value={password}
+    //     onChange={(e) => setPassword(e.target.value)}
+    //   />
+    //   <Button onClick={handleSignIn}>Sign In</Button>
+    // </Box>
+    <Box sx={{ flexGrow: 1, mt: 8 }}>
+      <Grid container spacing={1}>
+        <Grid item xs={6}>
+          <Item>
+            <Box sx={{ fontSize: "25px" }}>
+              WELCOME <br />
+              TO <br />
+              CMU <br />
+              COMMUNITY
+            </Box>
+            <CardMedia
+              sizes="medium"
+              component="img"
+              width={"100%"}
+              image={community}
+              alt="Paella dish"
+            />
+          </Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>
+            <Box
+              sx={{
+                mt: "20%",
+                display: "flex",
+                flexDirection: "column",
+                gap: 1,
+              }}
+            >
+              <Box sx={{ fontSize: "40px", mb: 5 }}>Log in</Box>
+              <TextField
+                id="email"
+                label="Email"
+                variant="outlined"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                id="password"
+                label="Password"
+                variant="outlined"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button
+                size="small"
+                sx={{
+                  display: "flex",
+                  width: "50px",
+                  backgroundColor: "primary.light",
+                  color: "white",
+                  "&:hover": {background:"primary.contrastText", color:"black"}
+                }}
+                onClick={handleSignIn}
+              >
+                Sign In
+              </Button>
+            </Box>
+          </Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
