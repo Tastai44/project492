9049b15ed5cb6b5cb173dbbfb6b748d85167d236
Tastai44 <tastai_kh@cmu.ac.th>
2023-12-09 08:55:40 +0700

    add function resize file

--- src/components/Profile/ProCoverImage.tsx
+++ src/components/Profile/ProCoverImage.tsx
@@ -72,12 +72,79 @@ export default function ProCoverImage() {
         fetchImages();
     }, [reFresh]);
 
-    const handleUpload = async () => {
+    // const handleUpload = async () => {
+    //     setLopenLoading(true);
+    //     if (imageUpload == null) return;
+    //     const fileName = removeSpacesBetweenWords(imageUpload.name);
+    //     const imageRef = ref(storage, `Images/${userId}${fileName}`);
+    //     uploadBytes(imageRef, imageUpload).then(() => {
+    //         handleEditPhotoProfile(`${userId}${imageUpload.name}`);
+    //     });
+    // };
+    const handleUpload = async (): Promise<void> => {
         setLopenLoading(true);
         if (imageUpload == null) return;
+
+        // Function to resize the image
+        const resizeImage = (file: File, maxWidth: number, maxHeight: number): Promise<Blob> => {
+            return new Promise((resolve) => {
+                const image = new Image();
+                const reader = new FileReader();
+
+                reader.onload = (readerEvent) => {
+                    image.onload = () => {
+                        const canvas = document.createElement('canvas');
+                        const ctx = canvas.getContext('2d');
+
+                        let width = image.width;
+                        let height = image.height;
+
+                        // Calculate the new dimensions to maintain aspect ratio
+                        if (width > height) {
+                            if (width > maxWidth) {
+                                height *= maxWidth / width;
+                                width = maxWidth;
+                            }
+                        } else {
+                            if (height > maxHeight) {
+                                width *= maxHeight / height;
+                                height = maxHeight;
+                            }
+                        }
+
+                        // Resize the image
+                        canvas.width = width;
+                        canvas.height = height;
+                        ctx?.drawImage(image, 0, 0, width, height);
+
+                        // Convert the canvas to a Blob
+                        canvas.toBlob((blob) => {
+                            if (blob) {
+                                resolve(blob);
+                            } else {
+                                throw new Error('Error converting canvas to Blob.');
+                            }
+                        }, file.type);
+                    };
+
+                    image.src = readerEvent.target?.result as string;
+                };
+
+                reader.readAsDataURL(file);
+            });
+        };
+
+        // Set the maximum dimensions for the resized image
+        const maxWidth = 800;
+        const maxHeight = 600;
+
+        // Resize the image before uploading
+        const resizedImage = await resizeImage(imageUpload, maxWidth, maxHeight);
+
+        // Now you can upload the resized image
         const fileName = removeSpacesBetweenWords(imageUpload.name);
         const imageRef = ref(storage, `Images/${userId}${fileName}`);
-        uploadBytes(imageRef, imageUpload).then(() => {
+        uploadBytes(imageRef, resizedImage).then(() => {
             handleEditPhotoProfile(`${userId}${imageUpload.name}`);
         });
     };
